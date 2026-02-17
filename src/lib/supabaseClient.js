570752b4af5b38ae75f56e904134a6c57c7adab9
Darling Rebolledo <darliina.rr@gmail.com>
2026-02-17 00:29:53 -0300

    fix: move supabaseClient into src/lib for alias @/

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -5,6 +5,3 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-supabaseClient.js
