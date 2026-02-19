82cd04bd8a5a9993bb0d0d0d20649c51e4189bb9
Darling Rebolledo <darliina.rr@gmail.com>
2026-02-19 01:18:32 -0300

    new changess

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import CardioMetabolicApp from "../CardioMetabolicApp";
+import dynamic from "next/dynamic";
+
+const CardioMetabolicApp = dynamic(() => import("../CardioMetabolicApp"), {
+  ssr: false,
+});
 
 export default function QuizPage() {
   return <CardioMetabolicApp />;
